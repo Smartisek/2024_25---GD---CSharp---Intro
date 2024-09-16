4849eafa7b3540fa709b93f94bbb3bab5e57e0e6
Smartisek <domalipdominik@gmail.com>
2024-09-16 11:14:33 +0100

    switch class

--- IntroToCSharp/IntroToCSharp/obj/Debug/net8.0/IntroToCSharp.AssemblyInfo.cs
+++ IntroToCSharp/IntroToCSharp/obj/Debug/net8.0/IntroToCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroToCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39be598d5e2022e9e18e56090511eb392a71213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39070302f5149999cf03b7e83944e7e1db442f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroToCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroToCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
