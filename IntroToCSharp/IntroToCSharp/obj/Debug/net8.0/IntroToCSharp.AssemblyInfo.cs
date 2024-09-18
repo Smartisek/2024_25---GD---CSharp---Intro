43b82e991fd6b0fa729339e9d53c5a6a65a53178
Smartisek <domalipdominik@gmail.com>
2024-09-18 14:17:50 +0100

    NewRepo

--- IntroToCSharp/IntroToCSharp/obj/Debug/net8.0/IntroToCSharp.AssemblyInfo.cs
+++ IntroToCSharp/IntroToCSharp/obj/Debug/net8.0/IntroToCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroToCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786ca69c785a2560ebae01364feeb30eb9557533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3723cf871360c9a0f6e99ec6663f50c719c1b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroToCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroToCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
